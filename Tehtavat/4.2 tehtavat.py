addc76fb53c6879e8f3799af57b2ccaad8485d63
heidihannelem <heidi-hannele.manninen@metropolia.fi>
2025-09-28 20:33:25 +0300

    Moduuli 04 kotitehtävät

---
+++ Tehtavat/4.2 tehtavat.py
@@ -0,0 +1,6 @@
+tuuma = float(input("Syötä mitta tuumissa: "))
+while tuuma >= 0:
+    print("Tuuma on senttimetreissä:", tuuma * 2.54)
+    tuuma = float(input("Syötä mitta tuumissa: "))
+
+print("Negatiivinen mitta")
